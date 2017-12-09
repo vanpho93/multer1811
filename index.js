0d12a775e829d67d397fe7109561d7508c60eb87
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-09 21:19:11 +0700

    xong

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const reload = require('reload');
-const upload = require('./uploadConfig');
+const uploadConfig = require('./uploadConfig');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.post('/signup', upload.single('image'), (req, res) => {
-    console.log(req.file);
-    console.log(req.body);
-    res.send('Thanh cong');
+const uploadSingle = uploadConfig.single('image');
+
+app.post('/signup', (req, res) => {
+    uploadSingle(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.file);
+        console.log(req.body);
+        res.send('Thanh cong');
+    });
+});
+
+const uploadArray = uploadConfig.array('image');
+app.post('/array', (req, res) => {
+    uploadArray(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.files);
+        console.log(req.body);
+        res.send('Thanh cong');
+    });
+});
+
+const uploadFields = uploadConfig.fields([
+    { name: 'image', maxCount: 2 },
+    { name: 'main', maxCount: 1 }
+]);
+
+app.post('/group', (req, res) => {
+    uploadFields(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.files);
+        console.log(req.body);
+        res.send('Thanh cong');
+    });
 });
 
 app.get('/', (req, res) => {
@@ -18,6 +47,14 @@ app.get('/', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+app.get('/array', (req, res) => {
+    res.render('array');
+});
+
+app.get('/group', (req, res) => {
+    res.render('group');
 });
 
 app.get('*', (req, res) => res.redirect('/'));
